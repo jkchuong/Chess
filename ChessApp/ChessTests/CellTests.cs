1ba0afc021b3315402286a60f17dcd4642ee033e
Jimmy Chuong <jkchuong@gmail.com>
2021-02-05 20:20:57 +0000

    Added extra cell test

--- ChessApp/ChessTests/CellTests.cs
+++ ChessApp/ChessTests/CellTests.cs
@@ -12,5 +12,12 @@ namespace ChessTests
             var cell = new Cell(x, y);
             Assert.AreEqual(expectedOutput, cell.ToString());
         }
+
+        [TestCase(2, 5, false)]
+        public void CellIsUnoccupiedOnCreation(int x, int y, bool expectedOutput)
+        {
+            var cell = new Cell(x, y);
+            Assert.AreEqual(expectedOutput, cell.IsOccupied);
+        }
     }
 }
