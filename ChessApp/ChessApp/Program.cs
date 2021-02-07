a4f1624cf28709581f229efad5c754c328831047
jkchuong <jkchuong@gmail.com>
2021-02-07 21:42:46 +0000

    I can't remember what I changed here

--- ChessApp/ChessApp/Program.cs
+++ ChessApp/ChessApp/Program.cs
@@ -7,10 +7,14 @@ namespace ChessApp
         static void Main(string[] args)
         {
             Chessboard chessboard = new Chessboard();
-            Console.WriteLine(chessboard.Board);
+
             Pawn pawn = new Pawn(true, chessboard.Board[0, 1]);
+
             string pawnMoves = pawn.GetPossibleMoves();
-            Console.WriteLine(pawnMoves);
+            string pawnPosition = pawn.Position.ToString();
+            Console.WriteLine($"Possible Moves: {pawnMoves}");
+            Console.WriteLine($"Position: {pawnPosition}");
+
         }
     }
 }
